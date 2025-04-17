b7e50f08b68e6f29db30dbda960f1c1c0d00b75d
niroshilon <nir.shilon@gmail.com>
2025-04-17 21:16:53 +0300

    tidy up code

--- lib/utils/cn.ts
+++ lib/utils/cn.ts
@@ -1,5 +1,5 @@
+import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { clsx, type ClassValue } from 'clsx';
 
 /**
  * Utility function to merge class names with Tailwind classes
